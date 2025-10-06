05459cbf188635da69e79f424f9eba600fc1c76b
Konrad <kobielus63@gmail.com>
2025-10-06 18:06:31 +0200

    akcje dla zakładek tab

--- pyqt/pyqt.py
+++ pyqt/pyqt.py
@@ -3,6 +3,7 @@ from PyQt6.QtWidgets import QLabel
 from PyQt6.QtWidgets import QMainWindow
 from PyQt6.QtWidgets import QStatusBar
 from PyQt6.QtWidgets import QToolBar
+from PyQt6.QtWidgets import QFileDialog
 from PyQt6.QtGui import QIcon, QAction
 
 # Tworzenie klasy głównego okna aplikacji dziedziczącej po QMainWindow
@@ -23,14 +24,20 @@ class Window(QMainWindow):
         self.menu = self.menuBar()
         # Dodanie do paska listy rozwijalnej o nazwie File
         self.fileMenu = self.menu.addMenu("File")
-        self.fileMenu = self.menu.addMenu("Task 1")
-        self.fileMenu = self.menu.addMenu("Task 2")
-        self.fileMenu = self.menu.addMenu("Task 3")
-        # Dodanie do menu File pozycji zamykającej aplikacje
         self.actionExit = QAction("Exit", self)
         self.actionExit.setShortcut("Ctrl+Q")
         self.actionExit.triggered.connect(self.close)
         self.fileMenu.addAction(self.actionExit)
+
+        self.task1Menu = self.menu.addMenu("Task 1")
+        self.actionOpen = QAction("Open file", self)
+        self.actionOpen.setShortcut("Ctrl+O")
+        self.actionOpen.triggered.connect(self.openImageDialog)
+        self.task1Menu.addAction(self.actionOpen)
+
+        self.task2Menu = self.menu.addMenu("Task 2")
+        self.task3Menu = self.menu.addMenu("Task 3")
+        # Dodanie do menu File pozycji zamykającej aplikacje
 
     # Funkcja dodająca wenętrzeny widżet do okna
     def createTabs(self):
@@ -50,6 +57,9 @@ class Window(QMainWindow):
         # Dodanie widżetu do głównego okna jako centralny widżet
         self.setCentralWidget(self.tabs)
 
+        self.tab_1.layout = None
+        self.tab_1.layout = QGridLayout()
+
         self.tab_2.layout = None
         self.tab_2.layout = QGridLayout()
         self.tab_2.layout.addWidget(QLabel("Zawartość drugiej zakładki"), 0, 0)
@@ -61,6 +71,16 @@ class Window(QMainWindow):
     # Funkcja obsługująca kliknięcie przycisku na pasku narzędzi
     def onMyToolBarButtonClick(self):
         self.statusBar().showMessage("Kliknięto przycisk na pasku narzędzi")
+
+    def openImageDialog(self):
+        file_dialog = QFileDialog(self)
+        file_dialog.setNameFilter("Images (*.png *.jpg *.jpeg *.bmp *.gif)")
+        if file_dialog.exec():
+            selected_files = file_dialog.selectedFiles()
+            if selected_files:
+                image_path = selected_files[0]
+                # Tu możesz dodać kod do wyświetlania obrazu w zakładce 1
+                self.statusBar().showMessage(f"Wybrano plik: {image_path}")
 
 
 # Uruchomienie okna
